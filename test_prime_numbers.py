8d828d00b3a26e76858cc78c98e980174e10465b
probuse <nprobuse@gmail.com>
2017-04-19 16:52:52 +0300

    update

--- test_prime_numbers.py
+++ test_prime_numbers.py
@@ -1,37 +1,23 @@
 import unittest
-from prime_numbers_ import prime_numbers
+from prime_numbers_ import prime_numbers, is_prime
 
 class PrimeNumberTestCases(unittest.TestCase):
-    def setup(self):
-        self.result1 = prime_numbers(-3)
-        self.result2 = prime_numbers(kkk)
-        self.result3 = prime_numbers('kkk')
-        self.result4 = prime_numbers(3.5)
-        self.result5 = prime_numbers(5)
-        self.result6 = prime_numbers(0)
-        self.result7 = prime_numbers(1)
-        self.result8 = prime_numbers(27)
-        self.result9 = prime_numbers(2)
     
-    def test_number_is_prime(self):
+    def test_prime_numbers_output(self):
         self.assertEquals(prime_numbers(6), [2, 3, 5])
         self.assertEquals(prime_numbers(2), [2])
-'''           
-    def test_negative_integer(self):
-        self.assertEquals(prime_numbers(-3), TypeError)
-        self.assertFalse(prime_numbers(-100))
-
+           
+    def test_input_is_positive_and_greater_than_zero(self):
+        with self.assertRaises(TypeError) : prime_numbers(-3)
+        with self.assertRaises(TypeError) : prime_numbers(0)
         
-    def test_only_integers(self):
-        self.assertIsInstance(prime_numbers(3.4), int)
-        self.assertFalse(prime_numbers('kkk'))
+    def test_input_is_integer(self):
+        with self.assertRaises(TypeError) : prime_numbers('Hello Friend')
+        with self.assertRaises(TypeError) : prime_numbers(3.45)
         
-    def test_one_and_zero(self):
-        self.assertEquals(prime_numbers(0), ValueError)
-        self.assertFalse(prime_numbers(1))
+    def test_1_not_prime(self):
+        self.assertEquals(prime_numbers(1), False)
         
-    def test_number_is_prime(self):
-        self.assertEquals(prime_numbers(6), [2, 3, 5])
-'''        
+    
 if __name__ == '__main__':
     unittest.main()
